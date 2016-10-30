679d9289f4eaba4ce1f555c4774acb5d801e9721
GUNEY <mehmetzahitt@gmail.com>
2016-10-30 21:12:57 +0300

    Configured The All Project Structures

---
+++ social-responsibility-web/src/main/java/com/mehmetzahit/contorller/test.java
@@ -0,0 +1,63 @@
+package com.mehmetzahit.contorller;
+
+import java.util.Scanner;
+
+/**
+ * Created by GUNEY on 23.10.2016.
+ */
+public class test {
+    public static void main(String[] args) {
+        int a=2,b=7,c=3;
+
+        int d1=a-b;
+        int d2=b-c;
+
+
+
+//        Scanner scanner=new Scanner(System.in);
+//
+//        a=scanner.nextInt();
+//        b=scanner.nextInt();
+//        c=scanner.nextInt();
+
+        if(a>b && a>c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if(a>b && a>c){
+            System.out.println(a);
+            if (b>c) System.out.println(b);
+            else System.out.println(c);
+        }
+
+        if(b>a && b>c){
+            System.out.println(b);
+        }
+        if(c>a && c>b){
+            System.out.println(c);
+        }
+
+
+
+
+    }
+}
